69e48f37e7a04d12fda3b597ce5ec328f2d84796
Muhammad Ahmed <ahmede7th@gmail.com>
2018-07-24 13:53:46 -0400

    Bootstrap/Reactstrap

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,22 +1,46 @@
 import React, { Component } from 'react';
-
+import { Button } from 'reactstrap';
 
 class Home extends Component {
   constructor() {
     super();
-    this.state = {}
+    this.state = {
+      error: null,
+      isLoaded: false,
+      data: []
+    }
+  this.handleClick = this.handleClick.bind(this);
+}
 
-  this.buttonClick = this.buttonClick.bind(this);
+componentDidMount(){
+fetch("https://randomuser.me/api/?results=500")
+.then(res => res.json())
+.then(
+  (result) => {
+    this.setState({
+      isLoaded: true,
+      data: result.results
+    });
+  },
+  (error) => {
+    this.setState({
+      isLoaded: true,
+      error
+    })
+  }
+)
 }
 
 
-buttonClick(){}
+handleClick(){
+  console.log("Success")
+}
 
 render() {
-
   return(
     <div>
       <h1>Home Component</h1>
+      <Button color="primary">primary</Button>
 
     </div>
   )
