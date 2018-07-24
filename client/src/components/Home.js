21373b63ce513ee4fdbc8bd09f1102e2fdbd3fa9
Muhammad Ahmed <ahmede7th@gmail.com>
2018-07-24 12:59:47 -0400

    Create-React-App, Home Component

---
+++ client/src/components/Home.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {}
+
+  this.buttonClick = this.buttonClick.bind(this);
+}
+
+
+buttonClick(){}
+
+render() {
+
+  return(
+    <div>
+      <h1>Home Component</h1>
+
+    </div>
+  )
+}
+}
+export default Home;
